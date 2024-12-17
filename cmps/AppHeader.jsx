ba8dff7aedfc369ff1e7be78bf6e6e27a1ce714f
seangah8 <seangah8@gmail.com>
2024-12-17 02:32:12 +0200

    adding progress precentage to the header

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,4 +1,4 @@
-const { useState } = React
+const { useState, useEffect } = React
 const { Link, NavLink } = ReactRouterDOM
 const { useNavigate } = ReactRouter
 
@@ -7,20 +7,40 @@ import { UserMsg } from "./UserMsg.jsx"
 import { LoginSignup } from './LoginSignup.jsx'
 import { showErrorMsg } from '../services/event-bus.service.js'
 import { logout } from '../store/actions/user.actions.js'
+import { loadTodos } from '../store/actions/todo.actions.js'
+
 
 const { useSelector } = ReactRedux
-
 
 
 export function AppHeader() {
     const navigate = useNavigate()
     const user = useSelector(storeState => storeState.userModule.loggedInUser)
+    const todos = useSelector(storeState => storeState.todoModule.todos)
 
+    useEffect(()=>{
+        loadTodos()
+    },[])
+    
+    
+    function todosPrecentage(){
+        if(todos.length <= 0) return 0
+        let doneTodos = 0
+        for(let i=0; i < todos.length; i++){
+            if(todos[i].isDone) doneTodos ++
+        }
+        return parseInt((doneTodos/todos.length) * 100)
+    }
 
     return (
         <header className="app-header full main-layout">
             <section className="header-container">
                 <h1>React Todo App</h1>
+
+                <section>
+                    <p>Todos Progress: {todosPrecentage()}%</p>
+                </section>
+
                 {user ? (
                     < section >
 
@@ -32,6 +52,9 @@ export function AppHeader() {
                         <LoginSignup/>
                     </section>
                 )}
+
+                
+
                 <nav className="app-nav">
                     <NavLink to="/" >Home</NavLink>
                     <NavLink to="/about" >About</NavLink>
